16b64e6a93f2bc017de5cb97fc6bfd0e4f590c1e
Mohamad Dzul Syakimin <dzulsyakimin61@gmail.com>
2024-06-26 22:07:52 +0800

    chore: Update environment variable for development base URL

--- src/shared/api/instance.tsx
+++ src/shared/api/instance.tsx
@@ -2,7 +2,7 @@ import axios from "redaxios";
 
 const createInstance = (token: string) =>
   axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: import.meta.env.VITE_BASE_URL as string,
     headers: {
       Authorization: `Bearer ${token}`,
     },
